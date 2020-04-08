8ca61d1541ce5f9870cb33247af4929db1d9cbd5
Rizky <spondbob@eamca.com>
2020-04-08 11:39:38 +1000

    match signed headers

--- src/getSignedUrl.ts
+++ src/getSignedUrl.ts
@@ -6,9 +6,7 @@ const storage = new Storage()
 export default (metadata:Metadata, filePathWithExt:string, bucketName:string) => {
   const SECONDS = 10000
   const expires = Date.now() + 30 * SECONDS
-  const extensionHeaders = metadata ? {
-    extensionHeaders: {'x-goog-meta-data': JSON.stringify(metadata)}
-  } : {}
+  const metaHeader = metadata ? {'x-goog-meta-data': JSON.stringify(metadata)} : {}
 
   const myBucket = storage.bucket(bucketName)
   const bucketFile = myBucket.file(filePathWithExt)
@@ -16,7 +14,10 @@ export default (metadata:Metadata, filePathWithExt:string, bucketName:string) =>
     action: 'write',
     contentType: metadata.mimetype,
     expires,
-    ...extensionHeaders,
+    extensionHeaders: {
+      'content-type': metadata.mimetype,
+      ...metaHeader
+    },
     version: 'v4',
   })
 }
